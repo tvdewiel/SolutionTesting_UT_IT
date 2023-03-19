2a1843cb147130e9dee36b5c6c94da25cc6042ac
Tom Vande Wiele <tvdewiel@hotmail.com>
2023-03-19 11:55:01 +0100

    Add project files.

---
+++ BusinessLayer/BikeInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace BusinessLayer
+{
+    public class BikeInfo
+    {
+        public BikeInfo(int? id, string description, BikeType bikeType, int customerId, string customerDescription, double purchaseCost)
+        {
+            Id = id;
+            Description = description;
+            BikeType = bikeType;
+            Customer = (customerId, customerDescription);
+            PurchaseCost = purchaseCost;
+        }
+        public int? Id { get; set; }
+        public string Description { get; set; }
+        public BikeType BikeType { get; set; }
+        public double PurchaseCost { get; set; }
+        public (int id, string description) Customer { get; set; }//description : name (email)
+    }
+}
